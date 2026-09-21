d8be589bccfc36906b20568309e2a81707a305fb
Vo Thanh Tuyen <132963766+thanhtuyen662002@users.noreply.github.com>
2026-09-22 05:03:18 +0700

    test: make realtime xunit references formatter-stable

--- tests/Platform.Persistence.Tests/TaskStatusRealtimeIsolationTests.cs
+++ tests/Platform.Persistence.Tests/TaskStatusRealtimeIsolationTests.cs
@@ -1,10 +1,8 @@
-using Xunit;
-
 namespace MinhHuy.AIOffice.Platform.Persistence.Tests;
 
 public sealed class TaskStatusRealtimeIsolationTests
 {
-    [Fact]
+    [Xunit.Fact]
     public void CompanyGroup_IsDeterministicForSameAuthorityScope()
     {
         var tenantId = Guid.Parse("11111111-1111-1111-1111-111111111111");
@@ -13,11 +11,11 @@ public sealed class TaskStatusRealtimeIsolationTests
         var first = global::MinhHuy.AIOffice.Core.Api.Realtime.TaskStatusRealtime.CompanyGroup(tenantId, companyId);
         var second = global::MinhHuy.AIOffice.Core.Api.Realtime.TaskStatusRealtime.CompanyGroup(tenantId, companyId);
 
-        Assert.Equal(first, second);
-        Assert.Equal($"tenant:{tenantId:D}:company:{companyId:D}", first);
+        Xunit.Assert.Equal(first, second);
+        Xunit.Assert.Equal($"tenant:{tenantId:D}:company:{companyId:D}", first);
     }
 
-    [Fact]
+    [Xunit.Fact]
     public void CompanyGroup_IsolatesCompaniesWithinSameTenant()
     {
         var tenantId = Guid.Parse("11111111-1111-1111-1111-111111111111");
@@ -27,10 +25,10 @@ public sealed class TaskStatusRealtimeIsolationTests
         var groupA = global::MinhHuy.AIOffice.Core.Api.Realtime.TaskStatusRealtime.CompanyGroup(tenantId, companyA);
         var groupB = global::MinhHuy.AIOffice.Core.Api.Realtime.TaskStatusRealtime.CompanyGroup(tenantId, companyB);
 
-        Assert.NotEqual(groupA, groupB);
+        Xunit.Assert.NotEqual(groupA, groupB);
     }
 
-    [Fact]
+    [Xunit.Fact]
     public void CompanyGroup_IsolatesSameCompanyIdentifierAcrossTenants()
     {
         var tenantA = Guid.Parse("11111111-1111-1111-1111-111111111111");
@@ -40,61 +38,61 @@ public sealed class TaskStatusRealtimeIsolationTests
         var groupA = global::MinhHuy.AIOffice.Core.Api.Realtime.TaskStatusRealtime.CompanyGroup(tenantA, companyId);
         var groupB = global::MinhHuy.AIOffice.Core.Api.Realtime.TaskStatusRealtime.CompanyGroup(tenantB, companyId);
 
-        Assert.NotEqual(groupA, groupB);
+        Xunit.Assert.NotEqual(groupA, groupB);
     }
 
-    [Theory]
-    [InlineData("00000000-0000-0000-0000-000000000000", "22222222-2222-2222-2222-222222222222")]
-    [InlineData("11111111-1111-1111-1111-111111111111", "00000000-0000-0000-0000-000000000000")]
+    [Xunit.Theory]
+    [Xunit.InlineData("00000000-0000-0000-0000-000000000000", "22222222-2222-2222-2222-222222222222")]
+    [Xunit.InlineData("11111111-1111-1111-1111-111111111111", "00000000-0000-0000-0000-000000000000")]
     public void CompanyGroup_FailsClosedWhenAuthorityScopeIsIncomplete(string tenant, string company)
     {
-        Assert.Throws<ArgumentException>(() =>
+        Xunit.Assert.Throws<ArgumentException>(() =>
             global::MinhHuy.AIOffice.Core.Api.Realtime.TaskStatusRealtime.CompanyGroup(Guid.Parse(tenant), Guid.Parse(company)));
     }
 
-    [Fact]
+    [Xunit.Fact]
     public void Publisher_FailsClosedBeforeDispatchWhenTaskIdentityIsMissing()
     {
         var publisher = new global::MinhHuy.AIOffice.Core.Api.Realtime.SignalRTaskStatusPublisher(null!);
         var message = new global::MinhHuy.AIOffice.Core.Api.Realtime.TaskStatusChanged(Guid.Empty, "running", DateTimeOffset.UtcNow);
 
-        Assert.Throws<ArgumentException>(() =>
+        Xunit.Assert.Throws<ArgumentException>(() =>
             publisher.PublishTaskStatusAsync(Guid.NewGuid(), Guid.NewGuid(), message));
     }
 
-    [Fact]
+    [Xunit.Fact]
     public void Publisher_FailsClosedBeforeDispatchWhenStepIdentityIsMissing()
     {
         var publisher = new global::MinhHuy.AIOffice.Core.Api.Realtime.SignalRTaskStatusPublisher(null!);
         var message = new global::MinhHuy.AIOffice.Core.Api.Realtime.TaskStepStatusChanged(Guid.NewGuid(), Guid.Empty, "running", DateTimeOffset.UtcNow);
 
-        Assert.Throws<ArgumentException>(() =>
+        Xunit.Assert.Throws<ArgumentException>(() =>
             publisher.PublishStepStatusAsync(Guid.NewGuid(), Guid.NewGuid(), message));
     }
 
-    [Theory]
-    [InlineData("")]
-    [InlineData("   ")]
+    [Xunit.Theory]
+    [Xunit.InlineData("")]
+    [Xunit.InlineData("   ")]
     public void Publisher_FailsClosedBeforeDispatchWhenWorkerIdentityIsMissing(string workerId)
     {
         var publisher = new global::MinhHuy.AIOffice.Core.Api.Realtime.SignalRTaskStatusPublisher(null!);
         var message = new global::MinhHuy.AIOffice.Core.Api.Realtime.WorkerStatusChanged(Guid.NewGuid(), workerId, "running", DateTimeOffset.UtcNow);
 
-        Assert.Throws<ArgumentException>(() =>
+        Xunit.Assert.Throws<ArgumentException>(() =>
             publisher.PublishWorkerStatusAsync(Guid.NewGuid(), Guid.NewGuid(), message));
     }
 
-    [Theory]
-    [InlineData("", "approval_required")]
-    [InlineData("approval", "")]
-    [InlineData("   ", "approval_required")]
-    [InlineData("approval", "   ")]
+    [Xunit.Theory]
+    [Xunit.InlineData("", "approval_required")]
+    [Xunit.InlineData("approval", "")]
+    [Xunit.InlineData("   ", "approval_required")]
+    [Xunit.InlineData("approval", "   ")]
     public void Publisher_FailsClosedBeforeDispatchWhenAttentionMetadataIsIncomplete(string kind, string reasonCode)
     {
         var publisher = new global::MinhHuy.AIOffice.Core.Api.Realtime.SignalRTaskStatusPublisher(null!);
         var message = new global::MinhHuy.AIOffice.Core.Api.Realtime.TaskAttentionRequired(Guid.NewGuid(), kind, reasonCode, DateTimeOffset.UtcNow);
 
-        Assert.Throws<ArgumentException>(() =>
+        Xunit.Assert.Throws<ArgumentException>(() =>
             publisher.PublishAttentionRequiredAsync(Guid.NewGuid(), Guid.NewGuid(), message));
     }
 }
