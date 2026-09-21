e483e09237f70d8c8a9ec34110998d0244814be9
Vo Thanh Tuyen <132963766+thanhtuyen662002@users.noreply.github.com>
2026-09-22 03:01:50 +0700

    test: avoid alias formatting churn in realtime acceptance

--- tests/Platform.Persistence.Tests/TaskStatusRealtimeIsolationTests.cs
+++ tests/Platform.Persistence.Tests/TaskStatusRealtimeIsolationTests.cs
@@ -1,7 +1,6 @@
 extern alias CoreApi;
 
 using Xunit;
-using Realtime = CoreApi::MinhHuy.AIOffice.Core.Api.Realtime;
 
 namespace MinhHuy.AIOffice.Platform.Persistence.Tests;
 
@@ -13,8 +12,8 @@ public sealed class TaskStatusRealtimeIsolationTests
         var tenantId = Guid.Parse("11111111-1111-1111-1111-111111111111");
         var companyId = Guid.Parse("22222222-2222-2222-2222-222222222222");
 
-        var first = Realtime.TaskStatusRealtime.CompanyGroup(tenantId, companyId);
-        var second = Realtime.TaskStatusRealtime.CompanyGroup(tenantId, companyId);
+        var first = CoreApi::MinhHuy.AIOffice.Core.Api.Realtime.TaskStatusRealtime.CompanyGroup(tenantId, companyId);
+        var second = CoreApi::MinhHuy.AIOffice.Core.Api.Realtime.TaskStatusRealtime.CompanyGroup(tenantId, companyId);
 
         Assert.Equal(first, second);
         Assert.Equal($"tenant:{tenantId:D}:company:{companyId:D}", first);
@@ -27,8 +26,8 @@ public sealed class TaskStatusRealtimeIsolationTests
         var companyA = Guid.Parse("22222222-2222-2222-2222-222222222222");
         var companyB = Guid.Parse("33333333-3333-3333-3333-333333333333");
 
-        var groupA = Realtime.TaskStatusRealtime.CompanyGroup(tenantId, companyA);
-        var groupB = Realtime.TaskStatusRealtime.CompanyGroup(tenantId, companyB);
+        var groupA = CoreApi::MinhHuy.AIOffice.Core.Api.Realtime.TaskStatusRealtime.CompanyGroup(tenantId, companyA);
+        var groupB = CoreApi::MinhHuy.AIOffice.Core.Api.Realtime.TaskStatusRealtime.CompanyGroup(tenantId, companyB);
 
         Assert.NotEqual(groupA, groupB);
     }
@@ -40,8 +39,8 @@ public sealed class TaskStatusRealtimeIsolationTests
         var tenantB = Guid.Parse("44444444-4444-4444-4444-444444444444");
         var companyId = Guid.Parse("22222222-2222-2222-2222-222222222222");
 
-        var groupA = Realtime.TaskStatusRealtime.CompanyGroup(tenantA, companyId);
-        var groupB = Realtime.TaskStatusRealtime.CompanyGroup(tenantB, companyId);
+        var groupA = CoreApi::MinhHuy.AIOffice.Core.Api.Realtime.TaskStatusRealtime.CompanyGroup(tenantA, companyId);
+        var groupB = CoreApi::MinhHuy.AIOffice.Core.Api.Realtime.TaskStatusRealtime.CompanyGroup(tenantB, companyId);
 
         Assert.NotEqual(groupA, groupB);
     }
@@ -52,14 +51,14 @@ public sealed class TaskStatusRealtimeIsolationTests
     public void CompanyGroup_FailsClosedWhenAuthorityScopeIsIncomplete(string tenant, string company)
     {
         Assert.Throws<ArgumentException>(() =>
-            Realtime.TaskStatusRealtime.CompanyGroup(Guid.Parse(tenant), Guid.Parse(company)));
+            CoreApi::MinhHuy.AIOffice.Core.Api.Realtime.TaskStatusRealtime.CompanyGroup(Guid.Parse(tenant), Guid.Parse(company)));
     }
 
     [Fact]
     public void Publisher_FailsClosedBeforeDispatchWhenTaskIdentityIsMissing()
     {
-        var publisher = new Realtime.SignalRTaskStatusPublisher(null!);
-        var message = new Realtime.TaskStatusChanged(Guid.Empty, "running", DateTimeOffset.UtcNow);
+        var publisher = new CoreApi::MinhHuy.AIOffice.Core.Api.Realtime.SignalRTaskStatusPublisher(null!);
+        var message = new CoreApi::MinhHuy.AIOffice.Core.Api.Realtime.TaskStatusChanged(Guid.Empty, "running", DateTimeOffset.UtcNow);
 
         Assert.Throws<ArgumentException>(() =>
             publisher.PublishTaskStatusAsync(Guid.NewGuid(), Guid.NewGuid(), message));
@@ -68,8 +67,8 @@ public sealed class TaskStatusRealtimeIsolationTests
     [Fact]
     public void Publisher_FailsClosedBeforeDispatchWhenStepIdentityIsMissing()
     {
-        var publisher = new Realtime.SignalRTaskStatusPublisher(null!);
-        var message = new Realtime.TaskStepStatusChanged(Guid.NewGuid(), Guid.Empty, "running", DateTimeOffset.UtcNow);
+        var publisher = new CoreApi::MinhHuy.AIOffice.Core.Api.Realtime.SignalRTaskStatusPublisher(null!);
+        var message = new CoreApi::MinhHuy.AIOffice.Core.Api.Realtime.TaskStepStatusChanged(Guid.NewGuid(), Guid.Empty, "running", DateTimeOffset.UtcNow);
 
         Assert.Throws<ArgumentException>(() =>
             publisher.PublishStepStatusAsync(Guid.NewGuid(), Guid.NewGuid(), message));
@@ -80,8 +79,8 @@ public sealed class TaskStatusRealtimeIsolationTests
     [InlineData("   ")]
     public void Publisher_FailsClosedBeforeDispatchWhenWorkerIdentityIsMissing(string workerId)
     {
-        var publisher = new Realtime.SignalRTaskStatusPublisher(null!);
-        var message = new Realtime.WorkerStatusChanged(Guid.NewGuid(), workerId, "running", DateTimeOffset.UtcNow);
+        var publisher = new CoreApi::MinhHuy.AIOffice.Core.Api.Realtime.SignalRTaskStatusPublisher(null!);
+        var message = new CoreApi::MinhHuy.AIOffice.Core.Api.Realtime.WorkerStatusChanged(Guid.NewGuid(), workerId, "running", DateTimeOffset.UtcNow);
 
         Assert.Throws<ArgumentException>(() =>
             publisher.PublishWorkerStatusAsync(Guid.NewGuid(), Guid.NewGuid(), message));
@@ -94,8 +93,8 @@ public sealed class TaskStatusRealtimeIsolationTests
     [InlineData("approval", "   ")]
     public void Publisher_FailsClosedBeforeDispatchWhenAttentionMetadataIsIncomplete(string kind, string reasonCode)
     {
-        var publisher = new Realtime.SignalRTaskStatusPublisher(null!);
-        var message = new Realtime.TaskAttentionRequired(Guid.NewGuid(), kind, reasonCode, DateTimeOffset.UtcNow);
+        var publisher = new CoreApi::MinhHuy.AIOffice.Core.Api.Realtime.SignalRTaskStatusPublisher(null!);
+        var message = new CoreApi::MinhHuy.AIOffice.Core.Api.Realtime.TaskAttentionRequired(Guid.NewGuid(), kind, reasonCode, DateTimeOffset.UtcNow);
 
         Assert.Throws<ArgumentException>(() =>
             publisher.PublishAttentionRequiredAsync(Guid.NewGuid(), Guid.NewGuid(), message));
